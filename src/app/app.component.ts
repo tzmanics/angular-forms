59d7589b3af6ea624588ad511ee0f53423058e27
Tara Z. Manicsic <tzmanics@gmail.com>
2020-01-09 21:34:30 -0500

    builds out simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'angular-forms';
+export class AppComponent implements OnInit {
+  title = "angular-forms";
+  name: string;
+  email: string;
+  message: string;
+
+  constructor() {}
+  ngOnInit() {}
 }
